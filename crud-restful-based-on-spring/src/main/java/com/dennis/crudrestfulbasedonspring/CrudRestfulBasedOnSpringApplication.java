f30ced5bf2ba51034baa3d7caa00aca5a40ba1ea
AllenDu <15162678717@163.com>
2020-09-06 20:49:09 +0800

    Initial commit

---
+++ crud-restful-based-on-spring/src/main/java/com/dennis/crudrestfulbasedonspring/CrudRestfulBasedOnSpringApplication.java
@@ -0,0 +1,13 @@
+package com.dennis.crudrestfulbasedonspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRestfulBasedOnSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudRestfulBasedOnSpringApplication.class, args);
+    }
+
+}
---
+++ crud-restful-based-on-spring/src/test/java/com/dennis/crudrestfulbasedonspring/CrudRestfulBasedOnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennis.crudrestfulbasedonspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestfulBasedOnSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
